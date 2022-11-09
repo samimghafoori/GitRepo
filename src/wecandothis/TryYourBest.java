9831d0f51244580f7d25f723a1acdb847cc3c5c4
samimghafoori <samimghafoori@yahoo.com>
2022-11-08 21:04:48 -0800

    this for try your best

---
+++ src/wecandothis/TryYourBest.java
@@ -0,0 +1,13 @@
+package wecandothis;
+
+public class TryYourBest {
+
+	public static void main(String[] args) {
+		
+
+	}
+	public void neverGiveUp() {
+		System.out.println("try to learn hard coding");
+	}
+
+}
